a4d9af3ebc46b58b0c2bd2b4d44bdd40858c4143
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2024-03-11 17:55:09 +0200

    add burger menu

--- src/Components/Layouts/Sidebar/index.js
+++ src/Components/Layouts/Sidebar/index.js
@@ -1,17 +1,62 @@
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './styles.css'
 
-const index = () => {
+const Index = () => {
+	const [isDropdownOpen, setDropdownOpen] = useState(false)
+	const [isBurgerActive, setBurgerActive] = useState(false)
+
+	const toggleDropdown = () => {
+		setDropdownOpen(!isDropdownOpen)
+	}
+
+	const toggleBurger = () => {
+		setBurgerActive(!isBurgerActive)
+		setDropdownOpen(false)
+
+		const navigationElement = document.querySelector('.navigation')
+		if (isBurgerActive) {
+			navigationElement.classList.remove('active')
+		} else {
+			navigationElement.classList.add('active')
+		}
+	}
+
+	useEffect(() => {
+		const handleOutsideClick = event => {
+			if (
+				isDropdownOpen &&
+				!event.target.closest('.dropdown') &&
+				!event.target.closest('.burger')
+			) {
+				setDropdownOpen(false)
+				setBurgerActive(false)
+			}
+		}
+
+		document.addEventListener('click', handleOutsideClick)
+
+		return () => {
+			document.removeEventListener('click', handleOutsideClick)
+		}
+	}, [isDropdownOpen, isBurgerActive])
+
 	return (
 		<>
 			<nav className='nav'>
-				<ul className='navigation'>
+				<ul className={`navigation ${isBurgerActive ? 'active' : ''}`}>
 					<li>
 						<NavLink to='/info'>ПРО ФИРМУ</NavLink>
 					</li>
-					<li className='dropdown' id='dropdown-toggle'>
-						<button>КАК КУПИТЬ</button>
-						<ul className='dropdown-menu' id='dropdown-menu'>
+					<li
+						className={`dropdown ${isDropdownOpen ? 'open' : ''}`}
+						id='dropdown-toggle'
+					>
+						<button onClick={toggleDropdown}>КАК КУПИТЬ</button>
+						<ul
+							className={`dropdown-menu ${isDropdownOpen ? 'active' : ''}`}
+							id='dropdown-menu'
+						>
 							<li>
 								<NavLink to='/rules'>правила</NavLink>
 							</li>
@@ -32,11 +77,14 @@ const index = () => {
 				</ul>
 			</nav>
 
-			<div className='burger'>
+			<div
+				className={`burger ${isBurgerActive ? 'active' : ''}`}
+				onClick={toggleBurger}
+			>
 				<span></span>
 			</div>
 		</>
 	)
 }
 
-export default index
+export default Index
